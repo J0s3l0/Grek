2c5f054b665c0c0960ccf10e6611d36bc28765c4
Jose Luis <A01063645@itesm.mx>
2016-02-16 18:01:46 -0600

    Initial commit

---
+++ app/src/main/java/com/transicion/grek/grek/MainActivity.java
@@ -0,0 +1,59 @@
+package com.transicion.grek.grek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+/*
+        TextView tv_suma = (TextView) findViewById(R.id.tv_suma);
+        int resultado = 0, a = 5, b = 2;
+        resultado = a + b;
+        tv_suma.setText(String.valueOf(resultado));
+
+        TextView totalFrutas = (TextView) findViewById(R.id.tv_frutas);
+        int resultado2 = 0, manzanas = 6, naranjas = 4;
+        resultado2 = manzanas + naranjas;
+        totalFrutas.setText("Total de frutas= "+String.valueOf(resultado2)+"!!!\n");
+
+        int califEnrique = 79, califCarlos = 89, valor = 70;
+
+        if(79 < valor )
+        {
+            totalFrutas.append("Enrique sigue participando\n");
+        }
+        else if(79 < 80)
+        {
+            totalFrutas.append("Enrique tienes C\n");
+        }
+        else if(79 < 90)
+        {
+            totalFrutas.append("Enrique tienes B\n");
+        }
+        else if(79 < 100)
+        {
+            totalFrutas.append("Enrique tienes A\n");
+        }
+        else
+        {
+            totalFrutas.append("Calificación inválida\n");
+        }
+
+        if(califCarlos < valor)
+        {
+            totalFrutas.append("Carlos sigue participando\n");
+        }
+
+
+
+
+*/
+
+    }
+}
